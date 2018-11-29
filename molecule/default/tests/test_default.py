d59cc171ebdd0c6279d0010adef4075afbd4ccae
Paul Kremer <pkremer@spurious.biz>
2018-11-29 10:36:12 +0100

    remove pytest deprecation warning

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,8 +7,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_cherrymusic_conf_file(File):
-    f = File('/home/cherrymusic/.config/cherrymusic/cherrymusic.conf')
+def test_cherrymusic_conf_file(host):
+    f = host.file('/home/cherrymusic/.config/cherrymusic/cherrymusic.conf')
 
     assert f.exists
     assert f.user == 'cherrymusic'
